3e446708c4382406f634252574b18a6a3ceaf2e1
HakimDai <hakim69400@live.com>
2021-06-08 23:28:50 +0200

    First Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {GrindDataFormPageComponent} from "./grind-data/grind-data-form-page/grind-data-form-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [{path: '', component: GrindDataFormPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { GrindDataModule } from './grind-data/grind-data.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    GrindDataModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
